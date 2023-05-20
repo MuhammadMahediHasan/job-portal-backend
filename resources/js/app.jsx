acdcc6f0a606c10e79374774044d648cc767c515
MuhammadMahediHasan <mahadih727@gmai.com>
2023-05-20 15:01:43 +0600

    employee order

---
+++ resources/js/app.jsx
@@ -0,0 +1,9 @@
+import './bootstrap';
+import '../css/app.css'
+
+import ReactDOM from 'react-dom/client';
+import Dashboard from "@/Pages/Dashboard";
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <Dashboard />
+);
